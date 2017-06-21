6d2422fe1ce1a46728cd46bc60a2c8f10c947518
EllenHu <HuFolder@EllenHudeMacBook-Air.local>
2017-06-20 22:24:09 -0400

    DetailPage

--- pages/dishDetails/dishDetails.js
+++ pages/dishDetails/dishDetails.js
@@ -1,66 +1,76 @@
-// dishDetails.js
+//details.js
+//获取应用实例
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    id: ''
+    motto: 'Hello World',
+    userInfo: {},
+    currentDish: {},
+    cachedDishes: {},
+    dishDes: '',
+    dishTitle: '',
+    dishIng: '',
+    dishBrd: '',
+    dishDetail: {},
+    stepLength: '',
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    this.setData({id: options.id})
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
   
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
   },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    wx.showLoading({
+      title: '加载中',
+    })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+    setTimeout(function () {
+      wx.hideLoading()
+    }, 2000)
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+    app.getUserInfo(function(userInfo){
+      that.setData({
+        userInfo:userInfo
+      })
+    })
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+   wx.request({
+      url: 'https://apis.juhe.cn/cook/index',
+      data: {
+        parentid: '',
+        dtype: '',
+        cid: '1',
+        key: '6ce3928fd610ba3b99b6003b77f0b070',
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      method: "GET",
+      success: function (res) {
+        console.log(res.data)
+        const chosenOne = Math.floor(Math.random() * 10)
+        const firstDish = res.data.result.data[chosenOne]
+        const length = firstDish.steps.length
+        that.setData({
+          cachedDishes: res.data.result.data,
+          currentDish: firstDish,
+          dishDes: firstDish.imtro,
+          dishTitle: firstDish.title,
+          dishPic: firstDish.albums[0],
+          dishIng: firstDish.ingredients,
+          dishBrd: firstDish.burden,
+          dishDetail: firstDish.steps,
+          stepLength: length,
+          //dishStep: firstDish.steps,
+        })
+        wx.hideLoading()
+      },
+      fail: function (res) {
+        console.log('request failed')
+      },
+    })
   }
 })
